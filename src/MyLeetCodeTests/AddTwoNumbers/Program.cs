98aef661ed809e492acd910341bef7d071a83fea
TZJ <727117719@qq.com>
2021-06-18 13:37:41 +0800

    Add files via upload

---
+++ src/MyLeetCodeTests/AddTwoNumbers/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int count = 1568;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(
+                    "-----------------------------------------------------------------------------------");
+                var l1 = CreateList(20);
+                var l2 = CreateList(15);
+
+                DisPlay(l1);
+                DisPlay(l2);
+
+                DisPlay(AddTwoNumbers(l1, l2));
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"耗时：{stopwatch.ElapsedMilliseconds/count}");
+        }
+
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var firstNode = new ListNode();
+
+            var lastNode = firstNode;
+
+            int into = 0;
+
+            while (true)
+            {
+                bool b1 = l1 != null;
+                bool b2 = l2 != null;
+
+                ListNode node;
+                if (!(b1 || b2))
+                {
+                    if (into > 0)
+                    {
+                        node = new ListNode(into);
+                        lastNode.next = node;
+                    }
+
+                    break;
+                }
+
+                int value = into;
+                if (b1)
+                {
+                    value += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (b2)
+                {
+                    value += l2.val;
+                    l2 = l2.next;
+                }
+
+                into = value / 10;
+
+                node = new ListNode(value % 10);
+                lastNode.next = node;
+                lastNode = node;
+            }
+
+            return firstNode.next;
+        }
+
+
+        private static void DisPlay(ListNode listNode)
+        {
+            Console.WriteLine($"[{string.Join(',', GetValues(listNode))}]");
+        }
+
+        private static IEnumerable<int> GetValues(ListNode listNode)
+        {
+            while (listNode != null)
+            {
+                yield return listNode.val;
+                listNode = listNode.next;
+            }
+        }
+
+        private static ListNode CreateList(int len)
+        {
+            ListNode l = new ListNode();
+
+            ListNode lastNode = l;
+            var random = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                lastNode.next = new ListNode(random.Next(9));
+                lastNode = lastNode.next;
+            }
+
+            return l.next;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/MyLeetCodeTests/TwoSum/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int len = (int)Math.Pow(10, 4);
+            int absValue = (int)Math.Pow(10, 9);
+
+            var nums = new int[len];
+            int target = 0;
+
+            int min = -absValue - 1;
+            int max = absValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = random.Next(min, max);
+            }
+
+            do
+            {
+                int first = random.Next(len);
+                int second = random.Next(len);
+
+                if (first == second)
+                    continue;
+
+                target = nums[first] + nums[second];
+
+                //if(absValue>=num>=absValue){}
+            } while (target > absValue || target < -absValue);
+
+
+            Console.WriteLine($"输入：");
+            Console.WriteLine($"nums:[{string.Join(',', nums)}]");
+            Console.WriteLine($"target:{target}");
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int[] result = TwoSum(nums, target);
+            for (int i = 0; i < 1000; i++)
+            {
+                result = TwoSum(nums, target);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"耗时：{stopwatch.ElapsedMilliseconds}");
+
+            if (result.Length == 0)
+                Console.WriteLine("未找到对应得值");
+            else
+            {
+                Console.WriteLine($"答案[{result[0]},{result[1]}]");
+                Console.WriteLine($"对应的值[{nums[result[0]]},{nums[result[1]]}]");
+            }
+
+            Console.Read();
+        }
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            var len = nums.Length;
+
+            var set = new Dictionary<int, int>();
+
+            for (int i = 0; i < len; i++)
+            {
+                var num = nums[i];
+
+                int sNum = target - num;
+                if (set.ContainsKey(sNum))
+                {
+                    return new[] { set[sNum], i };
+                }
+
+                if (!set.ContainsKey(num))
+                    set.Add(num, i);
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
