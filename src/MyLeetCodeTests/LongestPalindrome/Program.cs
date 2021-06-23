8f6941f90afffa4a630ff61781be7d495f9d4c6b
TianZhenjie <TianZhenjie@DESKTOP-HTNHDO9>
2021-06-23 18:08:45 +0800

    5

--- src/MyLeetCodeTests/LongestPalindrome/Program.cs
+++ src/MyLeetCodeTests/LongestPalindrome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LongestPalindrome
 {
@@ -6,7 +7,47 @@ namespace LongestPalindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var s = "babad";
+            s = "cbbd";
+            s = "a";
+            s = "ac";
+            s = "";
+
+            Console.WriteLine(LongestPalindrome(s));
+        }
+
+
+        public static string LongestPalindrome(string s)
+        {
+
+            if (string.IsNullOrWhiteSpace(s))
+                return s;
+            
+            var set = new Dictionary<char, int>();
+
+            var len = s.Length;
+
+            int start = 0, length = 0;
+            for (var i = 0; i < len; i++)
+            {
+                var c = s[i];
+                if (set.ContainsKey(c))
+                {
+                    var index = set[c];
+
+                    if (length < i - index)
+                    {
+                        length = i - index;
+                        start = index;
+                    }
+
+                    set.Remove(c);
+                }
+
+                set.Add(c, i);
+            }
+
+            return s.Substring(start, length + 1);
         }
     }
 }
