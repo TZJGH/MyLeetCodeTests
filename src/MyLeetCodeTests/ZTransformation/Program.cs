2ac212b33a34cdaa70a7bd2370adb1bde7647e37
TianZhenjie <TianZhenjie@DESKTOP-HTNHDO9>
2021-08-03 19:51:16 +0800

    Z字形变换

---
+++ src/MyLeetCodeTests/ZTransformation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZTransformation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = "PAYPALISHIRING";
+            string str = Convert(s, 3);
+
+            Console.WriteLine(str);
+        }
+
+        public static string Convert(string s, int numRows)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+                return s;
+
+            if (numRows == 1)
+                return s;
+
+            var r = new StringBuilder[numRows];
+            for (var i = 0; i < numRows; i++)
+            {
+                r[i] = new StringBuilder();
+            }
+
+            var len = s.Length;
+            var numRowsIndex = numRows - 1;
+            var pageSize = 2 * numRowsIndex;
+
+            for (var i = 0; i < len; i++)
+            {
+                var rowIndex = numRowsIndex - Math.Abs(i % pageSize + 1 - numRows);
+
+                r[rowIndex].Append(s[i]);
+            }
+
+            var sb = new StringBuilder(len);
+            foreach (var t in r)
+            {
+                sb.Append(t);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/MyLeetCodeTests/ZTransformation/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZTransformation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZTransformation")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("47BB6C64-2881-4672-9428-45DF29685AD7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
